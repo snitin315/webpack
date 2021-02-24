ffa81c8f2313e30eccf3239140569247564ae5b4
Nitin Kumar <snitin315@gmail.com>
2021-02-24 18:26:23 +0530

    export type Externals

--- types.d.ts
+++ types.d.ts
@@ -11556,6 +11556,7 @@ declare namespace exports {
 		Entry,
 		EntryNormalized,
 		EntryObject,
+		Externals,
 		LibraryOptions,
 		ModuleOptions,
 		ResolveOptionsWebpackOptions as ResolveOptions,
